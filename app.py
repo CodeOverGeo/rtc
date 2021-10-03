14d392dd2b7a06d288c56b5744fec5e5656402cb
Giovanni Rufino <giovannirufino@gmail.com>
2021-10-03 10:12:26 -0400

    attempt to fix heroku postgres to postgresql bug

--- app.py
+++ app.py
@@ -12,8 +12,12 @@ import os
 CURR_USER_KEY = 'curr_user'
 
 app = Flask(__name__, static_url_path='/static')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///rtc')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
